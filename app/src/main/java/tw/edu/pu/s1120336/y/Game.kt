692911cd7222343766914bd1ca458281c0803567
student <jing050916@gmail.com>
2024-11-25 16:16:25 +0800

    20241125

--- app/src/main/java/tw/edu/pu/s1120336/y/Game.kt
+++ app/src/main/java/tw/edu/pu/s1120336/y/Game.kt
@@ -1,14 +1,25 @@
 package tw.edu.pu.s1120336.y
 
+import android.content.Context
+import android.icu.number.Scale
+import android.media.MediaPlayer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
-class Game(val scope: CoroutineScope, val screenW:Int, val screenH: Int) {
+class Game(val scope: CoroutineScope, val screenW:Int, val screenH: Int,scale:Float, context: Context) {
+    //背景音樂
+    var mper1 = MediaPlayer.create(context, R.raw.lastletter)
+    //結束音樂
+    var mper2 = MediaPlayer.create(context, R.raw.gameover)
+
     var counter = 0
     val state = MutableStateFlow(0)
     val background = Background(screenW)
+
+    val boy = Boy(screenH,scale)
+    val virus = Virus(screenW, screenH, scale)
 
 
     var isPlaying = true
@@ -18,8 +29,22 @@ class Game(val scope: CoroutineScope, val screenW:Int, val screenH: Int) {
             //counter = 0
             isPlaying = true
             while (isPlaying) {
+                mper1.start()  //播放背景音樂
                 delay(40)
                 background.Roll()
+                if (counter % 3 == 0) {
+                    boy.Walk()
+                    virus.Fly()
+                    //判斷是否碰撞，結束遊戲
+                    if(boy.getRect().intersect(virus.getRect())) {
+                        isPlaying = false
+                        //遊戲結束音效
+                        mper1.pause()
+                        mper2.start()
+
+                    }
+
+                }
 
                 counter++
                 state.emit(counter)
@@ -27,4 +52,11 @@ class Game(val scope: CoroutineScope, val screenW:Int, val screenH: Int) {
             }
         }
     }
+    fun Restart(){
+        virus.Reset()
+        counter = 0
+        isPlaying = true
+        Play()
+    }
+
 }
